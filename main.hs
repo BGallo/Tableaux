5cb2502bb862647ce00e6fbbc3004004c4a40665
Matheus <matheusll@id.uff.br>
2022-11-16 07:33:49 -0300

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,15 +1,15 @@
-data Dado = Dado { value :: String
+data Data = Data { value :: String
                  , isop  :: Bool
                  , eval  :: Bool
                  } deriving Show
 data 
 
-create :: String -> Node
+create :: String -> Data
 create value
-    | value == "^" || value == "v" || value == "->" = Node {value = value, isop = True, eval = True}
-    | otherwise = Node {value = value, isop = False, eval = True}
+    | value == "^" || value == "v" || value == "->" = Data {value = value, isop = True, eval = True}
+    | otherwise = Data {value = value, isop = False, eval = True}
 
-build :: [Node] -> [Node]
+build :: [Data] -> [Data]
 build op
     | is == True  = [val, [build (tail op), build (tail (tail op))]] -- o mundo se isso funcionasse https://media.tenor.com/YHZLVDh63dwAAAAd/futuristic-city-star-trek-discovery.gif
     | otherwise = [val]
