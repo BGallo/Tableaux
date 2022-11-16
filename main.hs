b0a4f80e891fdf59acf2c0361e04295dfe984516
hcaeryks <matheusll@id.uff.br>
2022-11-16 01:21:29 -0300

    Painge

--- main.hs
+++ main.hs
@@ -1,7 +1,22 @@
-source = "-> a b"
+data Dado = Dado { value :: String
+                 , isop  :: Bool
+                 , eval  :: Bool
+                 } deriving Show
+data 
 
-b = words source
+create :: String -> Node
+create value
+    | value == "^" || value == "v" || value == "->" = Node {value = value, isop = True, eval = True}
+    | otherwise = Node {value = value, isop = False, eval = True}
 
-data Tree = Leaf String Bool | Node String Bool Tree Tree deriving (Show)
+build :: [Node] -> [Node]
+build op
+    | is == True  = [val, [build (tail op), build (tail (tail op))]] -- o mundo se isso funcionasse https://media.tenor.com/YHZLVDh63dwAAAAd/futuristic-city-star-trek-discovery.gif
+    | otherwise = [val]
+    where val = op !! 0
+          is  = isop  (op !! 0)
 
-main = putStrLn (b!!2)
+main = do
+    let source = words "-> a b"
+    let a = map create source
+    print(build a)
