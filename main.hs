c37aa37b7ca8548a7cf95b00fd9e7cc081a33678
Bernardo Gallo Victorino dos Santos <bgallo@id.uff.br>
2022-11-18 17:22:43 -0300

    sadness and sorrow

--- main.hs
+++ main.hs
@@ -1,15 +1,23 @@
+import Data.List
+--import Data.Tree
+import GHC.Maybe
+
 type Var    = String
-data Op = Nao | SeEntao | E | Ou | NaoOperador
-data Formula = Formula{operador :: Op, operandoUm::Formula,operandoDois::Formula,nome::String,valor::Bool} 
+data Op = Nao | SeEntao | E | Ou | NaoOperador deriving(Eq)
+data Formula = Formula{operador :: Op, operandoUm:: Maybe Formula,operandoDois:: Maybe Formula,nome::String,valor::Bool} 
 
-verdadeiro :: formula f ->formula x
+data Folha = Folha{valorVerdade::Bool,foiVisitado::Bool,forma::String}
+data Tree arvore = EmptyTree | Node arvore (Tree arvore) (Tree arvore) deriving(Read,Eq,Show)
+
+verdadeiro :: Formula  -> Maybe Formula 
 verdadeiro f
-    | operador f == NaoOperador = Formula{operador=NaoOperador,operandoUm= operandoUm f,operandoDois= operandoDois f,nome=nome f,valor = True} 
-    | operador f == Nao = falsificar Formula{operador=NaoOperador,operandoUm= foperandoUm f,operandoDois= operandoDois f,nome=nome f,valor =  valor  }
+    | operador f == NaoOperador = Formula{operador=NaoOperador,operandoUm= case operandoUm f of Just a -> operandoUm f,operandoDois= operandoDois f,nome=nome f,valor = True} 
+    | operador f == Nao = falsear Formula{operador=NaoOperador,operandoUm= operandoUm f,operandoDois= operandoDois f,nome=nome f,valor =  valor f }
 
-falsificar :: formula f->formula x
+falsear :: Formula -> Maybe Formula 
 
-falsificar f 
+falsear f 
     | operador f == NaoOperador = Formula{operador=NaoOperador,operandoUm= operandoUm f,operandoDois= operandoDois f,nome=nome f,valor = False} 
-    | operador f == Nao = Formula{operador=Nao,operandoUm= foperandoUm f,operandoDois= operandoDois f,nome=nome f,valor = ! valor f } 
-    | operador f == SeEntao = 
+    | operador f == Nao = verdadeiro Formula{operador=NaoOperador,operandoUm= operandoUm f,operandoDois= operandoDois f,nome=nome f,valor =  valor f } 
+    | operador f == SeEntao = Formula{operador=operador (operandoDois  f),operandoUm= verdadeiro (operandoUm f),operandoDois= falsear(operandoDois f),nome=nome f,valor =  valor f }
+
