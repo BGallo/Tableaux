58eaf6f1e0f0669aa295deb68163a7f47b3018cd
hcaeryks <matheusll@id.uff.br>
2022-11-22 13:53:41 -0300

    print infix tree

--- main.hs
+++ main.hs
@@ -3,6 +3,7 @@ import Data.Map
 import qualified Distribution.Simple.Program.HcPkg as Data
 import qualified Data.Sequence as Data.Map
 data Tree = Leaf {op :: String, eval :: Bool} | DBranch {op :: String, eval :: Bool, left :: Tree, right :: Tree} | SBranch {op :: String, eval :: Bool, left :: Tree} deriving Show
+
 buildTree :: String -> Tree
 buildTree formula = head $ Prelude.foldl pmatcher [] $ reverse $ words $ formula
     where pmatcher(x:y:ys) "^" = (DBranch{op = "^", eval = True, left = x, right = y}):ys
@@ -10,6 +11,7 @@ buildTree formula = head $ Prelude.foldl pmatcher [] $ reverse $ words $ formula
           pmatcher(x:y:ys) "->" = (DBranch{op = "->", eval = True, left = x, right = y}):ys
           pmatcher(x:xs) "~" = (SBranch{op = "~", eval = True, left = x}):xs
           pmatcher xs operand = (Leaf{op = operand, eval = True}):xs
+
 vTree :: Tree -> Tree
 vTree tree
     | opr == "^" = tree{eval = True, left = vTree $ left tree, right = vTree $ right tree}-- REGRA 3
@@ -18,6 +20,7 @@ vTree tree
     | opr == "~" = tree{eval = True, left = fTree $ left tree} -- REGRA 7
     | otherwise = tree{eval = True}
     where opr = op tree
+
 fTree :: Tree -> Tree
 fTree tree
     | opr == "^" = tree{eval = False,left = fTree $ left tree,right = fTree $ right tree } -- TODO -- REGRA 4
@@ -26,14 +29,11 @@ fTree tree
     | opr == "~" = tree{eval = False, left = vTree $ left tree} -- REGRA 8
     | otherwise = tree{eval = False}
     where opr = op tree
---branchCalc :: Tree -> Int -- calculates value of n in 2^n amount of branches
---branchCalc tree
---branchList :: Tree -> [Tree]
---branchList tree
+
 boolToString :: Bool -> String
 boolToString True = "TRUE"
 boolToString False = "FALSE"
---evalBranches :: Tree -> IO ()
+
 avaliar :: Tree->[String]->String
 avaliar t lista  
     |(( ( case opr `elemIndex` lista of 
@@ -53,21 +53,35 @@ avaliar t lista
 idk :: String -> [String]
 idk str = removeall "\n" $ removeall ":" $ words str
     where removeall val list = [ x | x <- list, x /= val ]
-    
 
 pUtil :: String -> String -> Map String String-> Map String String
 pUtil x y z = Data.Map.insert x y z 
 
- 
-
 aParser :: [String] -> Map String String-> String
- 
 aParser x y 
     | (Data.List.length x == 0) = "" 
-    |((Data.List.length x  /= 2 ) == False) = " A formula e valida \n " 
+    | ((Data.List.length x  /= 2 ) == False) = " A formula e valida \n " 
     | ((member (head x) y)  && not ( y Data.Map.! (head x) == (x!!2))) = " A formula e valida \n " 
     | otherwise =  aParser (Data.List.drop 2 x)  (Data.Map.insert (head x) (x !! 2)  y)
 
+lSubtreeIsOp :: Tree -> Bool
+lSubtreeIsOp tree
+    | opr == "^" || opr == "v" || opr == "->" = True
+    | otherwise = False
+    where opr = op (left tree)
+
+rSubtreeIsOp :: Tree -> Bool
+rSubtreeIsOp tree
+    | opr == "^" || opr == "v" || opr == "->" = True
+    | otherwise = False
+    where opr = op (right tree)
+
+treeToInfix :: Tree -> String
+treeToInfix tree
+    | opr == "^" || opr == "v" || opr == "->" = (if lSubtreeIsOp tree then "(" ++ treeToInfix (left tree) ++  ")" else treeToInfix (left tree)) ++ " " ++ opr ++ " " ++ (if rSubtreeIsOp tree then "(" ++ treeToInfix (right tree) ++  ")" else treeToInfix (right tree))
+    | opr == "~" = opr ++ " " ++ (if lSubtreeIsOp tree then "(" ++ treeToInfix (left tree) ++  ")" else treeToInfix (left tree))
+    | otherwise = opr
+    where opr = op tree
 
 treeToStr :: Tree -> Int -> String
 treeToStr tree count
@@ -89,4 +103,5 @@ main = do
     let result = avaliar fArvore []
     let aux = idk result
     let b = aParser aux (empty)
-    if b == "" then putStrLn result else  (putStrLn $ " A formula e valida \n" ++ (treeToStr  fArvore 1) ) 
+    putStrLn $ "Fórmula entrada: " ++ treeToInfix arvore
+    if b == "" then putStrLn result else  (putStrLn $ "A formula é valida. \n" ++ (treeToStr  fArvore 1) ) 
